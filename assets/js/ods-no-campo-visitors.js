e6beae48e147619ff1d8a5c70c680f63794e1ae8
Leandro de Mattos Pereira <mattoslmp@gmail.com>
2026-06-24 19:44:59 -0300

    Ativar relatório em tempo real da escuta

--- assets/js/ods-no-campo-visitors.js
+++ assets/js/ods-no-campo-visitors.js
@@ -20,6 +20,7 @@
   loadStyle('/assets/css/ods-no-campo-agro-data.css?v=20260624-2', 'data-ods-agro-data-css');
   loadStyle('/assets/css/ods-no-campo-biodiversity-ai.css?v=20260624-1', 'data-ods-biodiversity-ai-css');
   loadStyle('/assets/css/ods-no-campo-quiz-stats.css?v=20260624-2', 'data-ods-quiz-stats-css');
+  loadStyle('/assets/css/ods-no-campo-listening-report.css?v=20260624-1', 'data-ods-listening-report-css');
 
   loadScript('https://cdn.jsdelivr.net/gh/DatabiomicsAI/databiomicsai.github.io@ff5eaa478a2cf78fe93dba382a0496d98b90d997/assets/js/ods-no-campo-visitors.js', 'data-ods-visitors-original');
   loadScript('/assets/js/ods-no-campo-agro-data.js?v=20260624-2', 'data-ods-agro-data-js');
@@ -27,4 +28,5 @@
   loadScript('/assets/js/ods-no-campo-quiz-store.js?v=20260624-1', 'data-ods-quiz-store-js');
   loadScript('/assets/js/ods-no-campo-quiz-ui.js?v=20260624-1', 'data-ods-quiz-ui-js');
   loadScript('/assets/js/ods-no-campo-quiz-dashboard.js?v=20260624-1', 'data-ods-quiz-dashboard-js');
+  loadScript('/assets/js/ods-no-campo-listening-report.js?v=20260624-1', 'data-ods-listening-report-js');
 })();
