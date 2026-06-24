5b27d9d7f745f9d9e1ba0cd2f58a490687cad542
Leandro de Mattos Pereira <mattoslmp@gmail.com>
2026-06-24 12:08:38 -0300

    Atualizar cache visual do teste rápido

--- assets/js/ods-no-campo-visitors.js
+++ assets/js/ods-no-campo-visitors.js
@@ -19,7 +19,7 @@
 
   loadStyle('/assets/css/ods-no-campo-agro-data.css?v=20260624-2', 'data-ods-agro-data-css');
   loadStyle('/assets/css/ods-no-campo-biodiversity-ai.css?v=20260624-1', 'data-ods-biodiversity-ai-css');
-  loadStyle('/assets/css/ods-no-campo-quiz-stats.css?v=20260624-1', 'data-ods-quiz-stats-css');
+  loadStyle('/assets/css/ods-no-campo-quiz-stats.css?v=20260624-2', 'data-ods-quiz-stats-css');
 
   loadScript('https://cdn.jsdelivr.net/gh/DatabiomicsAI/databiomicsai.github.io@ff5eaa478a2cf78fe93dba382a0496d98b90d997/assets/js/ods-no-campo-visitors.js', 'data-ods-visitors-original');
   loadScript('/assets/js/ods-no-campo-agro-data.js?v=20260624-2', 'data-ods-agro-data-js');
