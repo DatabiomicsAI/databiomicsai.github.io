fd36dc1e327af1786b2593790327d666f37038d8
Leandro de Mattos Pereira <mattoslmp@gmail.com>
2026-06-24 11:04:28 -0300

    Ativar biodiversidade, Mata Atlântica, IA e gráficos de leite

--- assets/js/ods-no-campo-visitors.js
+++ assets/js/ods-no-campo-visitors.js
@@ -16,7 +16,9 @@
     document.head.appendChild(script);
   };
 
-  loadStyle('/assets/css/ods-no-campo-agro-data.css?v=20260624-1', 'data-ods-agro-data-css');
+  loadStyle('/assets/css/ods-no-campo-agro-data.css?v=20260624-2', 'data-ods-agro-data-css');
+  loadStyle('/assets/css/ods-no-campo-biodiversity-ai.css?v=20260624-1', 'data-ods-biodiversity-ai-css');
   loadScript('https://cdn.jsdelivr.net/gh/DatabiomicsAI/databiomicsai.github.io@ff5eaa478a2cf78fe93dba382a0496d98b90d997/assets/js/ods-no-campo-visitors.js', 'data-ods-visitors-original');
-  loadScript('/assets/js/ods-no-campo-agro-data.js?v=20260624-1', 'data-ods-agro-data-js');
+  loadScript('/assets/js/ods-no-campo-agro-data.js?v=20260624-2', 'data-ods-agro-data-js');
+  loadScript('/assets/js/ods-no-campo-biodiversity-ai.js?v=20260624-1', 'data-ods-biodiversity-ai-js');
 })();
