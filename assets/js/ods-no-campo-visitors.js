031c4ef7fd016cfe2638e918561ac8432d8658da
Leandro de Mattos Pereira <mattoslmp@gmail.com>
2026-06-25 12:21:14 -0300

    Ativar desafio interativo de perfil ODS

--- assets/js/ods-no-campo-visitors.js
+++ assets/js/ods-no-campo-visitors.js
@@ -21,6 +21,7 @@
   loadStyle('/assets/css/ods-no-campo-biodiversity-ai.css?v=20260624-1', 'data-ods-biodiversity-ai-css');
   loadStyle('/assets/css/ods-no-campo-quiz-stats.css?v=20260624-2', 'data-ods-quiz-stats-css');
   loadStyle('/assets/css/ods-no-campo-listening-report.css?v=20260624-2', 'data-ods-listening-report-css');
+  loadStyle('/assets/css/ods-no-campo-profile-challenge.css?v=20260625-1', 'data-ods-profile-challenge-css');
 
   loadScript('https://cdn.jsdelivr.net/gh/DatabiomicsAI/databiomicsai.github.io@ff5eaa478a2cf78fe93dba382a0496d98b90d997/assets/js/ods-no-campo-visitors.js', 'data-ods-visitors-original');
   loadScript('/assets/js/ods-no-campo-agro-data.js?v=20260624-2', 'data-ods-agro-data-js');
@@ -31,4 +32,5 @@
   loadScript('/assets/js/ods-no-campo-listening-report.js?v=20260624-1', 'data-ods-listening-report-js');
   loadScript('/assets/js/ods-no-campo-ods-ranking.js?v=20260624-1', 'data-ods-ranking-js');
   loadScript('/assets/js/ods-no-campo-credit-text-v2.js?v=20260625-3', 'data-ods-credit-text-v2-js');
+  loadScript('/assets/js/ods-no-campo-profile-challenge.js?v=20260625-1', 'data-ods-profile-challenge-js');
 })();
